7099d3ca8ca664fe581c245a1ba6142fa43c0dc0
santhosh <pitakasanthosh@gmail.com>
2023-09-28 16:22:10 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Home } from './components/Home'
+import Footer from './components/footer'
+
+import './css/App.css'
+
+function App() {
+
+  return (
+    <>
+    <Home />
+    <Footer />
+    </>
+  )
+}
+
+export default App
