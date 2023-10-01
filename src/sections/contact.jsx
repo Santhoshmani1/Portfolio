3521df57cfa2763dbaca498b3a923ad2c997d83d
santhosh <pitakasanthosh@gmail.com>
2023-10-01 11:33:41 +0530

    added contact section

---
+++ src/sections/contact.jsx
@@ -0,0 +1,96 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+import React from 'react';
+import { toast, ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
+export const Contact = () => {
+  return (
+    <>
+    <section id="contact">
+      <div id="contact-container">
+        <span className="material-icons">call</span>
+        <h2>Contact</h2>
+        <form action="post" id="contact-form">
+          <div className="form-group">
+            <input type="hidden" name="contact_number" />
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              className="form-control"
+              name="from_name"
+              placeholder="Enter your name"
+              required
+              minLength={5}
+              autoComplete="true"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              className="form-control"
+              name="from_email"
+              placeholder="enter Email address"
+              minLength={10}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="message">Message</label>
+            <textarea
+              className="form-control"
+              name="message"
+              minLength={10}
+              rows="10"
+              cols="40"
+            ></textarea>
+          </div>
+          <div className="button-container">
+            <button
+              type="submit"
+              className="btn primary-btn"
+              value="Send"
+              onClick={sendMail}
+            >
+              Submit
+            </button>
+            <ToastContainer autoClose={2000}  />
+          </div>{" "}
+        </form>
+      </div>
+      </section>
+    </>
+  );
+};
+
+
+const notify = () => {
+  toast.success("Success Notification !", {
+    position: toast.POSITION.BOTTOM_CENTER,
+    toastId: customId,
+    icon:"📨"
+  });
+}
+
+(function () {
+  emailjs.init("0jOAbCBLw9Ymv5HK8");
+})();
+
+function sendMail() {
+  document
+    .getElementById("contact-form")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+      this.contact_number.value = (Math.random() * 100000) | 0;
+      emailjs
+        .sendForm("service_portfolio_sandev", "template_f6l14fj", this)
+        .then(
+          function () {
+            console.log("success");
+          },
+          function (error) {
+            console.log("FAILED...", error);
+          }
+        );
+    });
+}
